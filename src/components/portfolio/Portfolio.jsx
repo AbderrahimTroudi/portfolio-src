ff5be06606558fca0ac1f958b1b85d20528bc5d4
AbderrahimTroudi <youssef2001-11@hotmail.com>
2022-11-21 15:03:51 +0100

    next1

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assest/portfolio1.jpg'
-import IMG2 from '../../assest/portfolio2.jpg'
-import IMG3 from '../../assest/portfolio3.jpg'
-import IMG4 from '../../assest/portfolio4.jpg'
-import IMG5 from '../../assest/portfolio5.png'
-import IMG6 from '../../assest/portfolio6.jpg'
+import IMG1 from '../../assest/javacapt.PNG'
+import IMG2 from '../../assest/portfolio1.jpg'
+import IMG3 from '../../assest/taxi.jpg'
+import IMG4 from '../../assest/portcap.PNG'
+
 const data = [
   {
     id:1,
     image:IMG1,
     title:'Java',
-    github:'https://github.com',
-    demo:'https://github.com'
-  },
-  
-  {
-    id:3,
-    image:IMG3,
-    title:'Spring boot',
-    github:'https://github.com',
-    demo:'https://github.com'
+    sousTitre:'',
+    github:'https://github.com/AbderrahimTroudi/Gestion-de-restaurant'
   },
   {
     id:4,
     image:IMG4,
     title:'React JS',
-    github:'https://github.com',
-    demo:'https://github.com'
+    
+    github:'https://github.com/AbderrahimTroudi/portfolio-src'
+  },
+  {
+    id:2,
+    image:IMG2,
+    title:'Power BI Dashboard',
+    sousTitre:'Currently working  on ',
+    github:'https://github.com'
+  },
+  {
+    id:3,
+    image:IMG3,
+    title:'Taxi reservation(Mobile application)',
+    sousTitre:'Currently working  on ',
+    github:'https://github.com'
   }
+  
 ]
 const Portfolio = () => {
   return (
@@ -38,15 +44,15 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="container port__container">
 {
-        data.map(({id,image,title,github,demo}) =>{
+        data.map(({id,image,title,github,sousTitre}) =>{
           return(<article key={id} className="port__item">
           <div className="port__item-image">
             <img src={image} alt={title} />
           </div>
           <h3>{title}</h3>
+          <h5>{sousTitre}</h5>
           <div className="port__item-cta">
           <a href={github} target="__blank" className="btn">GitHub</a>
-          <a href={demo} target="__blank" className="btn btn-primary">live Demo</a>
           </div>
         </article>)})
 }
